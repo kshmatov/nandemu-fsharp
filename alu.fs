01dcd8ee46d4ce436fef51cb2b10f8c3791cb926
Kirill Shmatov <kshmatov@gmail.com>
2024-05-09 19:16:10 +0300

    Fix for short pc (#7)
    * fixed
    
    * formatting

--- alu.fs
+++ alu.fs
@@ -12,7 +12,7 @@ let private dstMemMask = 0b1uy
 let private dstDRegisterMask = 0b10uy
 let private dstARegisterMask = 0b100uy
 
-type OpCode = 
+type OpCode =
     | zero = 0b101010
     | one = 0b111111
     | minusOne = 0b111010
@@ -33,7 +33,7 @@ type OpCode =
     | dOrSrc = 0b010101
     | halt = 0b011110
 
-type JumpCode = 
+type JumpCode =
     | None = 0b000
     | Gt = 0b001
     | Eq = 0b010
@@ -43,64 +43,58 @@ type JumpCode =
     | Le = 0b110
     | Now = 0b111
 
-let zeroOp (d: Value) (s:Value): Value = 0s
-let oneOp (d: Value) (s:Value): Value = 1s
-let minusOneOp (d: Value) (s:Value): Value  = -1s
-let dRegisterOp (d: Value) (s:Value): Value  = d
-let srcOp (d: Value) (s:Value): Value  = s
-let notDOp (d: Value) (s:Value): Value  = ~~~ d
-let notSrcOp (d: Value) (s:Value): Value  = ~~~ s
-let IncDOp (d: Value) (s:Value): Value  = d + 1s
-let IncSrcOp (d: Value) (s:Value): Value  = s + 1s
-let decDOp (d: Value) (s:Value): Value  = d - 1s
-let decSrcOp (d: Value) (s:Value): Value  = s - 1s
-let dPlusSrcOp (d: Value) (s:Value): Value  = d + s
-let dMinusSrcOp (d: Value) (s:Value): Value  = d - s
-let srcMinusDOp (d: Value) (s:Value): Value  = s - d
-let dAndSrcOp (d: Value) (s:Value): Value   = d &&& s
-let dOrSrcOp (d: Value) (s:Value): Value = d ||| s
-let haltOp (d: Value) (s:Value): Value = 0s
+let zeroOp (d: Value) (s: Value) : Value = 0s
+let oneOp (d: Value) (s: Value) : Value = 1s
+let minusOneOp (d: Value) (s: Value) : Value = -1s
+let dRegisterOp (d: Value) (s: Value) : Value = d
+let srcOp (d: Value) (s: Value) : Value = s
+let notDOp (d: Value) (s: Value) : Value = ~~~d
+let notSrcOp (d: Value) (s: Value) : Value = ~~~s
+let IncDOp (d: Value) (s: Value) : Value = d + 1s
+let IncSrcOp (d: Value) (s: Value) : Value = s + 1s
+let decDOp (d: Value) (s: Value) : Value = d - 1s
+let decSrcOp (d: Value) (s: Value) : Value = s - 1s
+let dPlusSrcOp (d: Value) (s: Value) : Value = d + s
+let dMinusSrcOp (d: Value) (s: Value) : Value = d - s
+let srcMinusDOp (d: Value) (s: Value) : Value = s - d
+let dAndSrcOp (d: Value) (s: Value) : Value = d &&& s
+let dOrSrcOp (d: Value) (s: Value) : Value = d ||| s
+let haltOp (d: Value) (s: Value) : Value = 0s
 
-type alu = {
-    mutable ProgramCounter: Address
-    mutable ARegister: Value
-    mutable DRegister: Value
-    Debug: bool
-}
+type alu =
+    { mutable ProgramCounter: Address
+      mutable ARegister: Value
+      mutable DRegister: Value
+      Debug: bool }
 
-type OperandSource = 
+type OperandSource =
     | Memory
     | ARegister
 
-type Destination = {
-    toMem:bool
-    toDRegister:bool
-    toARegister:bool
-}
+type Destination =
+    { toMem: bool
+      toDRegister: bool
+      toARegister: bool }
 
-type OpDesc = {
-    src: OperandSource
-    comp: OpCode
-    dest: Destination
-    jump: JumpCode
-}
+type OpDesc =
+    { src: OperandSource
+      comp: OpCode
+      dest: Destination
+      jump: JumpCode }
 
-type VMOp = 
+type VMOp =
     | AddressOp of Value
     | ComputationOp of OpDesc
 
 
-let isMask what mask =
-    what &&& mask = mask
+let isMask what mask = what &&& mask = mask
 
-let parseDest dst : Destination = 
-    {
-        toMem  = isMask dst dstMemMask
-        toDRegister = isMask dst dstDRegisterMask
-        toARegister = isMask dst dstARegisterMask
-    }
+let parseDest dst : Destination =
+    { toMem = isMask dst dstMemMask
+      toDRegister = isMask dst dstDRegisterMask
+      toARegister = isMask dst dstARegisterMask }
 
-let getOp (op:OpCode) =
+let getOp (op: OpCode) =
     match op with
     | OpCode.zero -> zeroOp
     | OpCode.one -> oneOp
@@ -122,76 +116,91 @@ let getOp (op:OpCode) =
     | OpCode.dOrSrc -> dOrSrcOp
     | _ -> haltOp
 
-let calcJump (jmp:JumpCode) (v: Value) (cp: Address) (alt:Address) :Address= 
+let calcJump (jmp: JumpCode) (v: Value) (cp: Address) (alt: Address) : Address =
     match jmp with
     | JumpCode.Gt when v > 0s -> alt
     | JumpCode.Eq when v = 0s -> alt
-    | JumpCode.Ge when v >= 0s-> alt
+    | JumpCode.Ge when v >= 0s -> alt
     | JumpCode.Lt when v < 0s -> alt
     | JumpCode.Ne when v <> 0s -> alt
     | JumpCode.Le when v <= 0s -> alt
     | JumpCode.Now -> alt
     | _ -> cp + 1us
 
-let parseInstruction (op: Instruction): VMOp = 
+let parseInstruction (op: Instruction) : VMOp =
     if op &&& firstBit = 0us then
-        AddressOp (op |> int16)
+        AddressOp(op |> int16)
     else
-        ComputationOp {
-            jump = jumpMask &&& op |> int32 |> enum<JumpCode>;
-            dest = destMask &&& op >>> 3 |> byte |> parseDest;
-            comp = compMask &&& op >>> 6 |> int32 |> enum<OpCode>;
-            src = if op &&& srcMask = 0us then OperandSource.ARegister else OperandSource.Memory
-        }
+        ComputationOp
+            { jump = jumpMask &&& op |> int32 |> enum<JumpCode>
+              dest = destMask &&& op >>> 3 |> byte |> parseDest
+              comp = compMask &&& op >>> 6 |> int32 |> enum<OpCode>
+              src =
+                if op &&& srcMask = 0us then
+                    OperandSource.ARegister
+                else
+                    OperandSource.Memory }
 
-let makeCOp (op: OpDesc) (regs: alu) (mem: State): alu =
+let makeCOp (op: OpDesc) (regs: alu) (mem: State) : alu =
     if regs.Debug then
-        printfn $"pc: {regs.ProgramCounter}; d: {regs.DRegister}; a: {regs.ARegister}; mem[a]:{mem.GetMem(regs.ARegister |> uint16)}"
+        printfn
+            $"pc: {regs.ProgramCounter}; d: {regs.DRegister}; a: {regs.ARegister}; mem[a]:{mem.GetMem(regs.ARegister |> uint16)}"
+
         printfn $"{op}"
-    let v = 
-        if op.src = OperandSource.Memory then 
+
+    let v =
+        if op.src = OperandSource.Memory then
             regs.ARegister |> uint16 |> mem.GetMem
-        else 
+        else
             regs.ARegister
+
     let res = getOp op.comp regs.DRegister v
-    if op.dest.toMem then mem.SetMem (regs.ARegister |> uint16) res
+
+    if op.dest.toMem then
+        mem.SetMem (regs.ARegister |> uint16) res
+
     regs.ProgramCounter <- calcJump op.jump res regs.ProgramCounter (regs.ARegister |> uint16)
     regs.ARegister <- if op.dest.toARegister then res else regs.ARegister
     regs.DRegister <- if op.dest.toDRegister then res else regs.DRegister
     regs
 
-let makeAOp(regs: alu) (a: Value): alu =
+let makeAOp (regs: alu) (a: Value) : alu =
     if regs.Debug then
         printfn $"pc: {regs.ProgramCounter}; d: {regs.DRegister}; a: {regs.ARegister}"
         printfn $"AddressOp old: {regs.ARegister} new: {a}"
+
     regs.ProgramCounter <- regs.ProgramCounter + 1us
     regs.ARegister <- a
     regs
 
-let oneStep (mem: State) (regs: alu):alu option= 
+let oneStep (mem: State) (regs: alu) : alu option =
     let op = regs.ProgramCounter |> mem.GetInstaruction |> parseInstruction
+
     match op with
-    | _ when regs.ProgramCounter > 0x7fffus -> None
+    | _ when regs.ProgramCounter > 0x7fffus -> None // Address is coded in 15bits
     | ComputationOp x when x.comp = OpCode.halt -> None
     | AddressOp a -> makeAOp regs a |> Some
     | ComputationOp op -> makeCOp op regs mem |> Some
 
 
-let InitAlu(debug:bool): alu = 
-    {ProgramCounter = 0us; ARegister = 0s; DRegister = 0s; Debug = debug}
+let InitAlu (debug: bool) : alu =
+    { ProgramCounter = 0us
+      ARegister = 0s
+      DRegister = 0s
+      Debug = debug }
 
-let RunAluSettings (rom: memory.Instruction array) (tick: int) (debug: bool): int = 
+let RunAluSettings (rom: memory.Instruction array) (tick: int) (debug: bool) : int =
     let regs = InitAlu debug
     let mem = memory.Init None
-    rom |> mem.Load 
+    rom |> mem.Load
 
-    let rec step (mem: State) (regs: alu):int = 
+    let rec step (mem: State) (regs: alu) : int =
         System.Threading.Thread.Sleep tick
+
         match oneStep mem regs with
         | None -> 0
         | Some regs -> step mem regs
 
     step mem regs
 
-let RunAlu (rom: memory.Instruction array) =
-    RunAluSettings rom 1 false
+let RunAlu (rom: memory.Instruction array) = RunAluSettings rom 1 false
--- computer.fs
+++ computer.fs
@@ -3,7 +3,7 @@ module computer
 open System
 open System.Windows.Forms
 
-type NandKeys = 
+type NandKeys =
     | Char of int
     | Esc
     | Space
@@ -22,7 +22,8 @@ type NandKeys =
     | Enter
     | Ignored
 
-let keyToCode = function
+let keyToCode =
+    function
     | NandKeys.Esc -> 140s
     | NandKeys.Func x -> 140 + x |> int16
     | NandKeys.BSpace -> 129s
@@ -37,115 +38,113 @@ let keyToCode = function
     | NandKeys.Left -> 130s
     | NandKeys.Right -> 132s
     | NandKeys.Enter -> 128s
-    | NandKeys.Char x ->  x |> int16
+    | NandKeys.Char x -> x |> int16
     | _ -> 0s
 
-let keyDown (ev: Keys): NandKeys =
+let keyDown (ev: Keys) : NandKeys =
     match ev with
-        | x when x = Keys.Back -> NandKeys.BSpace
-        | x when x = Keys.Escape || x = Keys.CapsLock -> NandKeys.Esc
-        | x when x >= Keys.F1 && x <= Keys.F12 -> int(ev) + 1 - int(Keys.F1) |> Func
-        | x when x = Keys.Delete -> NandKeys.Del
-        | x when x = Keys.Insert -> NandKeys.Ins
-        | x when x = Keys.Home -> NandKeys.Home
-        | x when x = Keys.End -> NandKeys.End
-        | x when x = Keys.PageUp -> NandKeys.PgUp
-        | x when x = Keys.PageDown -> NandKeys.PgDown
-        | x when x = Keys.Up -> NandKeys.Up
-        | x when x = Keys.Down -> NandKeys.Down
-        | x when x = Keys.Left -> NandKeys.Left
-        | x when x = Keys. Right -> NandKeys.Right
-        | x when x = Keys.Enter -> NandKeys.Enter
-        | _ -> NandKeys.Ignored
+    | x when x = Keys.Back -> NandKeys.BSpace
+    | x when x = Keys.Escape || x = Keys.CapsLock -> NandKeys.Esc
+    | x when x >= Keys.F1 && x <= Keys.F12 -> int (ev) + 1 - int (Keys.F1) |> Func
+    | x when x = Keys.Delete -> NandKeys.Del
+    | x when x = Keys.Insert -> NandKeys.Ins
+    | x when x = Keys.Home -> NandKeys.Home
+    | x when x = Keys.End -> NandKeys.End
+    | x when x = Keys.PageUp -> NandKeys.PgUp
+    | x when x = Keys.PageDown -> NandKeys.PgDown
+    | x when x = Keys.Up -> NandKeys.Up
+    | x when x = Keys.Down -> NandKeys.Down
+    | x when x = Keys.Left -> NandKeys.Left
+    | x when x = Keys.Right -> NandKeys.Right
+    | x when x = Keys.Enter -> NandKeys.Enter
+    | _ -> NandKeys.Ignored
 
-let drawByte (b: Drawing.Bitmap) (row: int) (col: int) (d: int16):unit =
+let drawByte (b: Drawing.Bitmap) (row: int) (col: int) (d: int16) : unit =
     for col1 = 0 to 15 do
         if (d <<< col1) &&& 0b1000000000000000s = 0s then
-            b.SetPixel(col*16+col1, row, Drawing.Color.Black)
+            b.SetPixel(col * 16 + col1, row, Drawing.Color.Black)
         else
-            b.SetPixel(col*16+col1, row, Drawing.Color.White)
+            b.SetPixel(col * 16 + col1, row, Drawing.Color.White)
 
-let updateDisplay (p: PictureBox) (bmp: Drawing.Bitmap): unit =
+let updateDisplay (p: PictureBox) (bmp: Drawing.Bitmap) : unit =
     let g = p.CreateGraphics()
-    g.DrawImage(bmp, 0, 0 ,512, 256)
+    g.DrawImage(bmp, 0, 0, 512, 256)
 
 
-let fullDisplayUpdate  (p: PictureBox) :unit =
-    let bmp =  p.Image :?> Drawing.Bitmap
+let fullDisplayUpdate (p: PictureBox) : unit =
+    let bmp = p.Image :?> Drawing.Bitmap
+
     for row = 0 to 255 do
         let pos = 32 * row
+
         for col = 0 to 511 do
             bmp.SetPixel(col, row, Drawing.Color.Black)
+
     updateDisplay p bmp
-    
-let byteDisplayUpdate (p: PictureBox): (memory.Address -> memory.Value -> unit) = 
+
+let byteDisplayUpdate (p: PictureBox) : (memory.Address -> memory.Value -> unit) =
     (fun a v ->
-        let bmp =  p.Image :?> Drawing.Bitmap
+        let bmp = p.Image :?> Drawing.Bitmap
         let row = a / 32us
         let col = a % 32us
         drawByte bmp (row |> int) (col |> int) v
-        updateDisplay p bmp
-    )
+        updateDisplay p bmp)
 
-let getDisplay() =
+let getDisplay () =
     let display = new PictureBox()
     display.BackColor <- Drawing.Color.Gray
-    display.Size  <- Drawing.Size(Drawing.Point(512,256))
+    display.Size <- Drawing.Size(Drawing.Point(512, 256))
     display.Top <- 2
     display.Left <- 2
     display.Enabled <- false
     display.Image <- new Drawing.Bitmap(512, 256)
     display
 
-let getForm (display: PictureBox):Form =
-    let form = new Form(Text="Nand to Tetris VM",
-                        Visible = true,
-                        TopMost = true)
+let getForm (display: PictureBox) : Form =
+    let form = new Form(Text = "Nand to Tetris VM", Visible = true, TopMost = true)
     form.FormBorderStyle <- FormBorderStyle.FixedSingle
-    form.ClientSize <- Drawing.Size(Drawing.Point(512,256))
+    form.ClientSize <- Drawing.Size(Drawing.Point(512, 256))
     form.MaximizeBox <- false
     form.Controls.Add display
     form.AutoSize <- true
     form
 
-let getTimer tick = 
+let getTimer tick =
     let timer = new Timer()
     timer.Interval <- tick
     timer
-    
-let RunComputer (debug: bool) (tick: int) (rom: uint16 array) = 
-    let display = getDisplay()
+
+let RunComputer (debug: bool) (tick: int) (rom: uint16 array) =
+    let display = getDisplay ()
     let form = getForm display
 
     let mutable regs = alu.InitAlu debug
-    let mem = memory.Init (byteDisplayUpdate display |> Some)
+    let mem = memory.Init(byteDisplayUpdate display |> Some)
     mem.Load rom
 
     form.Click.Add(fun evArgs -> System.Console.Beep())
-    form.KeyDown.Add( fun ev -> 
-        let x = keyDown ev.KeyCode 
+
+    form.KeyDown.Add(fun ev ->
+        let x = keyDown ev.KeyCode
+
         if x <> NandKeys.Ignored then
             x |> keyToCode |> mem.SetKey
-            ev.Handled <- true
-    )
-    form.KeyPress.Add( fun ev -> 
-        ev.KeyChar |> int16 |> mem.SetKey
-    )
+            ev.Handled <- true)
+
+    form.KeyPress.Add(fun ev -> ev.KeyChar |> int16 |> mem.SetKey)
     form.KeyUp.Add(fun ev -> mem.SetKey 0s)
 
     let timer = getTimer tick
-    
-    timer.Tick.Add(fun x  ->
-        match alu.oneStep mem regs with
-        | Some r -> 
-            regs <-r
-        | None -> 
-            timer.Enabled <- false
-            printfn "done"
-    )
 
-    form.Shown.Add(fun x -> 
+    timer.Tick.Add(fun x ->
+        match alu.oneStep mem regs with
+        | Some r -> regs <- r
+        | None ->
+            timer.Enabled <- false
+            printfn "done")
+
+    form.Shown.Add(fun x ->
         fullDisplayUpdate display
-        timer.Enabled <- true
-    )
+        timer.Enabled <- true)
+
     Application.Run(form)
